a825bd5c5b74414be39196185506261622b6f9f6
Leif Berg <lpberg@iastate.edu>
2012-02-09 12:33:41 -0600

    burr puzzle is troubling ...should probably redo this part, but wont

--- spartaApp/loadBurrPuzzle.lua
+++ spartaApp/loadBurrPuzzle.lua
@@ -3,9 +3,14 @@ params = defineSimulationParameters{
 	maxStiffness = 300.0
 	}
 
-scale = 5.0
+local scale = 5.0
+
+local deltaX = .25
+local deltaY = -.9
+local deltaZ = .12
 pieceBlue =Transform{
 	scale = 5.0,
+	position = {deltaX,deltaY,deltaZ},
 	Transform{
 		position = {-.1,.3,0},
 		orientation = AngleAxis(Degrees(90), Axis{0.0, 0.0, 1.0}),
@@ -14,6 +19,7 @@ pieceBlue =Transform{
 }
 pieceYellow = Transform{
 	scale = 5.0,
+	position = {deltaX,deltaY,deltaZ},
 	Transform{
 		position = {-.09,.29,-.099},
 		orientation = AngleAxis(Degrees(90), Axis{0.0, 1.0, 0.0}),
@@ -26,6 +32,7 @@ pieceYellow = Transform{
 }
 pieceGreen =Transform{
 	scale = 5.0,
+	position = {deltaX,deltaY,deltaZ},
 	 Transform{
 		position = {-.08999,.2999,0.011},
 		orientation = AngleAxis(Degrees(90), Axis{1.0, 0.0, 0.0}),
@@ -34,6 +41,7 @@ pieceGreen =Transform{
 }
 piecePurple = Transform{
 	scale = 5.0,
+	position = {deltaX,deltaY,deltaZ},
 	Transform{
 		position = {.22,.2,0},
 		orientation = AngleAxis(Degrees(90), Axis{0.0, 0.0, 1.0}),
@@ -46,6 +54,7 @@ piecePurple = Transform{
 }
 pieceTeal = Transform{
 	scale = 5.0,
+	position = {deltaX,deltaY,deltaZ},
 	Transform{
 		position = {-.0895,.3,-.009},
 		orientation = AngleAxis(Degrees(180), Axis{1.0, 0.0, 0.0}),
@@ -54,6 +63,7 @@ pieceTeal = Transform{
 }
 pieceRed = Transform{
 	scale = 5.0,
+	position = {deltaX,deltaY,deltaZ},
 	Transform{
 		position = {-.36/4,1.237/4,.0015/4},
 		orientation = AngleAxis(Degrees(90), Axis{0.0, 1.0, 0.0}),
@@ -61,41 +71,38 @@ pieceRed = Transform{
 	}
 }
 
-deltaX = .25
-deltaY = -.9
-deltaZ = .12
 blue = addObject{
-	position = {deltaX,deltaY,deltaZ},
+	position = {0,0,0},
 	voxelsize = 0.003,
 	density = 5,
 	pieceBlue,
 }
 yellow = addObject{
-	position = {deltaX,deltaY,deltaZ},
+	position = {0,0,0},
 	voxelsize = 0.003,
 	density = 5,
 	pieceYellow,
 }
 green = addObject{
-	position = {deltaX,deltaY,deltaZ},
+	position = {0,0,0},
 	voxelsize = 0.003,
 	density = 5,
 	pieceGreen,
 }
 purple = addObject{
-	position = {deltaX,deltaY,deltaZ},
+	position = {0,0,0},
 	voxelsize = 0.003,
 	density = 5,
 	piecePurple,
 }
 teal = addObject{
-	position = {deltaX,deltaY,deltaZ},
+	position = {0,0,0},
 	voxelsize = 0.003,
 	density = 10,
 	pieceTeal,
 }
 red = addObject{
-	position = {deltaX,deltaY,deltaZ},
+	position = {0,0,0},
 	voxelsize = 0.003,
 	density = 15,
 	pieceRed,
