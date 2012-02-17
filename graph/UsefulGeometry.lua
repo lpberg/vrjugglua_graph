597171c258120de0e489c343fe7b23387b78386e
Leif Berg <lpberg@iastate.edu>
2012-02-17 17:06:33 -0600

    supporting changes for color changing functionality

--- graph/UsefulGeometry.lua
+++ graph/UsefulGeometry.lua
@@ -1,11 +1,11 @@
-CylinderFromHereToThere = function(here, there,rad) 
+CylinderFromHereToThere = function(here, there,rad,mycolor) 
 	local Cylinder = function (a)
 		local pos = osg.Vec3(0.0, 0.0, 0.0)
 		if a.position then
 			pos:set(unpack(a.position))
 		end
 		local drbl = osg.ShapeDrawable(osg.Cylinder(pos, rad or 0.025, a.height or 1.0))
-		local color = osg.Vec4(0,0,1,0)
+		local color = osg.Vec4((105/255),(105/255),(105/255),0)
 		if a.color then
 			color:set(unpack(a.color))
 		end
@@ -14,11 +14,10 @@ CylinderFromHereToThere = function(here, there,rad)
 		geode:addDrawable(drbl)
 		return geode
 	end
-	
 	local midpoint = (here + there) * 0.5
 	local xform = Transform{
 		position = {midpoint:x(),midpoint:y(),midpoint:z()},
-		Cylinder{height = (here-there):length()},
+		Cylinder{height = (here-there):length(),color = mycolor},
 	}
 	local newVec = there - here
 	local newQuat = osg.Quat() -- Isn't there a static method like osg.Quat.rotate()? TODO
@@ -27,26 +26,26 @@ CylinderFromHereToThere = function(here, there,rad)
 	return xform
 end
 YellowCylinderFromHereToThere = function(here, there,rad) 
-	local function TransparentObject(group)
-		local state = group:getOrCreateStateSet()
-		state:setRenderingHint(2) -- transparent bin
+	-- local function TransparentObject(group)
+		-- local state = group:getOrCreateStateSet()
+		-- state:setRenderingHint(2) -- transparent bin
 
-		local CONSTANT_ALPHA = 0x8003
-		local ONE_MINUS_CONSTANT_ALPHA = 0x8004
-		local bf = osg.BlendFunc()
-		bf:setFunction(CONSTANT_ALPHA, ONE_MINUS_CONSTANT_ALPHA)
-		state:setAttributeAndModes(bf)
+		-- local CONSTANT_ALPHA = 0x8003
+		-- local ONE_MINUS_CONSTANT_ALPHA = 0x8004
+		-- local bf = osg.BlendFunc()
+		-- bf:setFunction(CONSTANT_ALPHA, ONE_MINUS_CONSTANT_ALPHA)
+		-- state:setAttributeAndModes(bf)
 
-		local bc = osg.BlendColor(osg.Vec4(1.0, 1.0, 1.0, arg.alpha or 0.5))
-		state:setAttributeAndModes(bc)
-		group:setStateSet(state)
-	end
+		-- local bc = osg.BlendColor(osg.Vec4(1.0, 1.0, 1.0, arg.alpha or 0.5))
+		-- state:setAttributeAndModes(bc)
+		-- group:setStateSet(state)
+	-- end
 	local Cylinder = function (a)
 		local pos = osg.Vec3(0.0, 0.0, 0.0)
 		if a.position then
 			pos:set(unpack(a.position))
 		end
-		local drbl = osg.ShapeDrawable(osg.Cylinder(pos, rad*2 or 0.025*2, a.height or 1.0))
+		local drbl = osg.ShapeDrawable(osg.Cylinder(pos, rad*3 or 0.025*3, a.height or 1.0))
 		local color = osg.Vec4(1,1,0,0)
 		if a.color then
 			color:set(unpack(a.color))
@@ -85,7 +84,7 @@ RedIndicatorSphere = function(myRadius)
 		bf:setFunction(CONSTANT_ALPHA, ONE_MINUS_CONSTANT_ALPHA)
 		state:setAttributeAndModes(bf)
 
-		local bc = osg.BlendColor(osg.Vec4(1.0, 1.0, 1.0,  0.5))
+		local bc = osg.BlendColor(osg.Vec4(1.0, 1.0, 1.0,  0.25))
 		state:setAttributeAndModes(bc)
 		group:setStateSet(state)
 	end
@@ -96,7 +95,7 @@ RedIndicatorSphere = function(myRadius)
 		end
 		local sphere = osg.Sphere(pos, a.radius or 1.0)
 		local drbl = osg.ShapeDrawable(sphere)
-		local color = osg.Vec4(1,1,1,0)
+		local color = osg.Vec4(0,0,0,1)
 		if a.color then
 			color:set(unpack(a.color))
 		end
@@ -112,4 +111,20 @@ RedIndicatorSphere = function(myRadius)
 		}
 	TransparentObject(xform)
 	return xform
+end
+ColorSphere = function(a)
+	local pos = osg.Vec3(0.0, 0.0, 0.0)
+	if a.position then
+		pos:set(unpack(a.position))
+	end
+	local sphere = osg.Sphere(pos, a.radius or 1.0)
+	local drbl = osg.ShapeDrawable(sphere)
+	local color = osg.Vec4(1,1,1,0)
+	if a.color then
+		color:set(unpack(a.color))
+	end
+	drbl:setColor(color)
+	local geode = osg.Geode()
+	geode:addDrawable(drbl)
+	return geode
 end
