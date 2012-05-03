4c7b8a8c5e0a3e0f32ae827fa95183e8b670ad2b
Leif Berg <lpberg@iastate.edu>
2012-05-03 14:43:42 -0500

    refactored useful geometry to reduce redundancy

--- graph/UsefulGeometry.lua
+++ graph/UsefulGeometry.lua
@@ -1,55 +1,51 @@
-CylinderFromHereToThere = function(here, there,rad,mycolor) 
-	local Cylinder = function (a)
-		local pos = osg.Vec3(0.0, 0.0, 0.0)
-		if a.position then
-			pos:set(unpack(a.position))
-		end
-		local drbl = osg.ShapeDrawable(osg.Cylinder(pos, rad or 0.025, a.height or 1.0))
-		local color = osg.Vec4((105/255),(105/255),(105/255),0)
-		if a.color then
-			color:set(unpack(a.color))
-		end
-		drbl:setColor(color)
-		local geode = osg.Geode()
-		geode:addDrawable(drbl)
-		return geode
-	end
+require("TransparentGroup")
+
+local Cylinder = function (a)
+	local pos = osg.Vec3(0.0, 0.0, 0.0) 
+	local drbl = osg.ShapeDrawable(osg.Cylinder(pos, a.radius, a.height))
+	local color = osg.Vec4(0,0,0,0)
+	color:set(unpack(a.color))
+	drbl:setColor(color)
+	local geode = osg.Geode()
+	geode:addDrawable(drbl)
+	return geode
+end
+
+ColorSphere = function(a)
+	local pos = osg.Vec3(0.0, 0.0, 0.0)
+	local sphere = osg.Sphere(pos, a.radius)
+	local drbl = osg.ShapeDrawable(sphere)
+	local color = osg.Vec4()
+	color:set(unpack(a.color))
+	drbl:setColor(color)
+	local geode = osg.Geode()
+	geode:addDrawable(drbl)
+	return geode
+end
+
+CylinderFromHereToThere = function(here, there, myradius,mycolor) 
+
 	local midpoint = (here + there) * 0.5
+	local myheight = (here-there):length()
+	
 	local xform = Transform{
 		position = {midpoint:x(),midpoint:y(),midpoint:z()},
-		Cylinder{height = (here-there):length(),color = mycolor},
+		Cylinder({height = myheight, color = mycolor, radius = myradius}),
 	}
 	local newVec = there - here
-	local newQuat = osg.Quat() -- Isn't there a static method like osg.Quat.rotate()? TODO
+	local newQuat = osg.Quat()
 	newQuat:makeRotate(osg.Vec3(0,0,1),osg.Vec3(newVec:x(),newVec:y(),newVec:z()))
 	xform:setAttitude(newQuat)
 	return xform
 end
-YellowCylinderFromHereToThere = function(here, there,rad) 
-	local Cylinder = function (a)
-		local pos = osg.Vec3(0.0, 0.0, 0.0)
-		if a.position then
-			pos:set(unpack(a.position))
-		end
-		local drbl = osg.ShapeDrawable(osg.Cylinder(pos, rad*3 or 0.025*3, a.height or 1.0))
-		local color = osg.Vec4(1,1,0,0)
-		if a.color then
-			color:set(unpack(a.color))
-		end
-		drbl:setColor(color)
-		local geode = osg.Geode()
-		geode:addDrawable(drbl)
-		
-		local xform = Transform{
-			geode,
-		}
-		return xform
-	end
+
+YellowCylinderFromHereToThere = function(here, there,radius) 
 	
 	local midpoint = (here + there) * 0.5
+	local myheight = (here-there):length()
 	local xform = Transform{
 		position = {midpoint:x(),midpoint:y(),midpoint:z()},
-		Cylinder{height = (here-there):length()},
+		Cylinder{height = myheight, radius = radius*3, color = {1,1,0,0}}
 	}
 	local newVec = there - here
 	local newQuat = osg.Quat() -- Isn't there a static method like osg.Quat.rotate()? TODO
@@ -60,57 +56,10 @@ YellowCylinderFromHereToThere = function(here, there,rad)
 end
 
 RedIndicatorSphere = function(myRadius) 
-	local function TransparentObject(group)
-		local state = group:getOrCreateStateSet()
-		state:setRenderingHint(2) -- transparent bin
-
-		local CONSTANT_ALPHA = 0x8003
-		local ONE_MINUS_CONSTANT_ALPHA = 0x8004
-		local bf = osg.BlendFunc()
-		bf:setFunction(CONSTANT_ALPHA, ONE_MINUS_CONSTANT_ALPHA)
-		state:setAttributeAndModes(bf)
-
-		local bc = osg.BlendColor(osg.Vec4(1.0, 1.0, 1.0,  0.25))
-		state:setAttributeAndModes(bc)
-		group:setStateSet(state)
-	end
-	local ColorSphere = function(a)
-		local pos = osg.Vec3(0.0, 0.0, 0.0)
-		if a.position then
-			pos:set(unpack(a.position))
-		end
-		local sphere = osg.Sphere(pos, a.radius or 1.0)
-		local drbl = osg.ShapeDrawable(sphere)
-		local color = osg.Vec4(0,0,0,1)
-		if a.color then
-			color:set(unpack(a.color))
-		end
-		drbl:setColor(color)
-		local geode = osg.Geode()
-		geode:addDrawable(drbl)
-		return geode
-	end
-
 	local xform = Transform{
 			position = {0,0,0},
-			ColorSphere{radius = myRadius, color = {1,0,0,1}},
+			TransparentGroup{ColorSphere{radius = myRadius, color = {1,0,0,1}}},
 		}
-	TransparentObject(xform)
 	return xform
 end
-ColorSphere = function(a)
-	local pos = osg.Vec3(0.0, 0.0, 0.0)
-	if a.position then
-		pos:set(unpack(a.position))
-	end
-	local sphere = osg.Sphere(pos, a.radius or 1.0)
-	local drbl = osg.ShapeDrawable(sphere)
-	local color = osg.Vec4(1,1,1,0)
-	if a.color then
-		color:set(unpack(a.color))
-	end
-	drbl:setColor(color)
-	local geode = osg.Geode()
-	geode:addDrawable(drbl)
-	return geode
-end
+
