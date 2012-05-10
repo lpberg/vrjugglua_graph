53652df2eb1fc37f38be0d97250c550952cc721d
unknown <lpberg@.vrac.iastate.edu>
2012-05-10 08:29:39 -0500

    cleaned and removed redundant code in usefulgeometry

--- graph/UsefulGeometry.lua
+++ graph/UsefulGeometry.lua
@@ -25,8 +25,7 @@ end
 local Cylinder = function (a)
 	local pos = osg.Vec3(0.0, 0.0, 0.0) 
 	local drbl = osg.ShapeDrawable(osg.Cylinder(pos, a.radius, a.height))
-	local color = osg.Vec4(0,0,0,0)
-	color:set(unpack(a.color))
+	local color = osg.Vec4(unpack(a.color))
 	drbl:setColor(color)
 	local geode = osg.Geode()
 	geode:addDrawable(drbl)
@@ -37,22 +36,19 @@ ColorSphere = function(a)
 	local pos = osg.Vec3(0.0, 0.0, 0.0)
 	local sphere = osg.Sphere(pos, a.radius)
 	local drbl = osg.ShapeDrawable(sphere)
-	local color = osg.Vec4()
-	color:set(unpack(a.color))
+	local color = osg.Vec4(unpack(a.color))
 	drbl:setColor(color)
 	local geode = osg.Geode()
 	geode:addDrawable(drbl)
 	return geode
 end
 
-CylinderFromHereToThere = function(here, there, myradius,mycolor) 
-
+CylinderFromHereToThere = function(here, there, myradius, mycolor) 
 	local midpoint = (here + there) * 0.5
 	local myheight = (here-there):length()
-	
 	local xform = Transform{
 		position = {midpoint:x(),midpoint:y(),midpoint:z()},
-		Cylinder({height = myheight, color = mycolor, radius = myradius}),
+		Cylinder{height = myheight, color = mycolor, radius = myradius},
 	}
 	local newVec = there - here
 	local newQuat = osg.Quat()
@@ -61,21 +57,6 @@ CylinderFromHereToThere = function(here, there, myradius,mycolor)
 	return xform
 end
 
-YellowCylinderFromHereToThere = function(here, there,radius) 
-	
-	local midpoint = (here + there) * 0.5
-	local myheight = (here-there):length()
-	local xform = Transform{
-		position = {midpoint:x(),midpoint:y(),midpoint:z()},
-		Cylinder{height = myheight, radius = radius*3, color = {1,1,0,0}}
-	}
-	local newVec = there - here
-	local newQuat = osg.Quat() -- Isn't there a static method like osg.Quat.rotate()? TODO
-	newQuat:makeRotate(osg.Vec3(0,0,1),osg.Vec3(newVec:x(),newVec:y(),newVec:z()))
-	xform:setAttitude(newQuat)
-	
-	return xform
-end
 
 RedIndicatorSphere = function(myRadius) 
 	local xform = Transform{
