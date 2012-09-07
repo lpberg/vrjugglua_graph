236cd313b2c8edeaa069d533fefe71d5d464fb7f
Leif Berg <lpberg@iastate.edu>
2012-09-07 16:40:46 -0500

    revamped edge osg hierarchy

--- graph/DirectedEdge.lua
+++ graph/DirectedEdge.lua
@@ -22,24 +22,50 @@ function DirectedEdgeIndex:createOSG()
 	--set up directedEdge variables
 	self.srcpos = self.src.position
 	self.destpos = self.dest.position
+	
 	--create "label" object
 	self.labelSwitch = osg.Switch()
 	self.labelSwitch:addChild(TextLabel(Vec(unpack(self.srcpos)), Vec(unpack(self.destpos)), self.labeltext, self.labelSize, self.radius, self.labelColor))
 	self.labelSwitch:setAllChildrenOff()
-	--create "edge" osg objects inside a switch
-	print("CreateOSG CAll - normal color: ",self.color)
-	print("CreateOSG CAll - highlighted Color: ",self.highlightColor)
+	--normal edge osg
+	self.osgcylinder = CylinderFromHereToThere(Vec(unpack(self.srcpos)), Vec(unpack(self.destpos)),self.radius,self.color)
+	--create a edge controller switch
+	self.edge_controll_switch = osg.Switch()
+	self.osg_elements = Transform{
+						self.osgcylinder,
+						self.labelSwitch
+						}
+	
+	self.transparent_low = TransparentGroup{alpha=.25}
+	self.transparent_high = TransparentGroup{alpha=.75}
+	self.transparent_low:addChild(self.osg_elements)
+	self.transparent_high:addChild(self.osg_elements)
+	
+	self.edge_controll_switch:addChild(self.osg_elements)
+	self.edge_controll_switch:addChild(self.transparent_low)
+	self.edge_controll_switch:addChild(self.transparent_high)
+	
 	self.indicators = osg.Switch()
-	self.indicators:addChild(CylinderFromHereToThere(Vec(unpack(self.srcpos)), Vec(unpack(self.destpos)),self.radius,self.color))
-	self.indicators:addChild(CylinderFromHereToThere(Vec(unpack(self.srcpos)), Vec(unpack(self.destpos)),self.radius*3,self.highlightColor))
-	self.indicators:setSingleChildOn(0)
+	self.indicators:addChild(CylinderFromHereToThere(Vec(unpack(self.srcpos)), Vec(unpack(self.destpos)),self.radius*1.1,self.highlightColor))
+	--highlight off by default
+	self.indicators:setAllChildrenOff()
 	--add both label and edge objects into main xform
 	self.osg = Transform{
 		self.indicators,
-		self.labelSwitch,
+		self.edge_controll_switch,
 	}
 end
 
+
+function DirectedEdgeIndex:setLowTransparency()
+	self.edge_controll_switch:setSingleChildOn(2)
+end
+function DirectedEdgeIndex:setHighTransparency()
+	self.edge_controll_switch:setSingleChildOn(1)
+end
+function DirectedEdgeIndex:setNoTransparency()
+	self.edge_controll_switch:setSingleChildOn(0)
+end
 function DirectedEdgeIndex:updateOSG()
 	if self.srcpos == self.src.position and self.destpos == self.dest.position then
 		--No need to update
@@ -49,22 +75,31 @@ function DirectedEdgeIndex:updateOSG()
 	self.srcpos = self.src.position
 	self.destpos = self.dest.position
 	-- update label graphic
-	self.labelSwitch.Child[1] = TextLabel(Vec(unpack(self.srcpos)), Vec(unpack(self.destpos)),self.labeltext,self.labelSize,self.radius,self.labelColor)
+	self.osg_elements.Child[2] = TextLabel(Vec(unpack(self.srcpos)), Vec(unpack(self.destpos)),self.labeltext,self.labelSize,self.radius,self.labelColor)
 	--update normal edge graphic
-	self.indicators.Child[1] = CylinderFromHereToThere(Vec(unpack(self.srcpos)), Vec(unpack(self.destpos)),self.radius,self.color)
+	osg_elements.Child[1] = CylinderFromHereToThere(Vec(unpack(self.srcpos)), Vec(unpack(self.destpos)),self.radius,self.color)
 	--update highlighted edge graphic
 	-- print(self.highlightedColor)
-	self.indicators.Child[2] = CylinderFromHereToThere(Vec(unpack(self.srcpos)), Vec(unpack(self.destpos)),self.radius*3,self.highlightColor)
+	self.indicators.Child[1] = CylinderFromHereToThere(Vec(unpack(self.srcpos)), Vec(unpack(self.destpos)),self.radius*1.1,self.highlightColor)
 	-- self.indicators.Child[2] = HighlightCylinderFromHereToThere(Vec(unpack(self.srcpos)), Vec(unpack(self.destpos)),self.radius,self.highlightColor)
+end
+
+function DirectedEdgeIndex:shrinkEdge()
+	self.radius = self.radius/2
+	self:updateOSG()
+end
+function DirectedEdgeIndex:expandEdge()
+	self.radius = self.radius*2
+	self:updateOSG()
 end
 
 function DirectedEdgeIndex:highlight(val)
 	if val then
 		--turn on yellow cylinder & everything else off
-		self.indicators:setSingleChildOn(1)
+		self.indicators:setAllChildrenOn()
 	else
 		--turn on normal cylinder & everything else off
-		self.indicators:setSingleChildOn(0)
+		self.indicators:setAllChildrenOff()
 	end
 end
 
@@ -80,7 +115,7 @@ DirectedEdge = function(source, destination,args)
 	-- setmetatable returns the table it is given after it modifies it by setting the metatable
 	-- so this is a commonly-seen pattern
 	local _radius = 0.007
-	local _color = {(105/255),(105/255),(105/255),0} --gray color default
+	local _color = {(105/255),(105/255),(105/255),1} --gray color default
 	local _labelColor = {1,1,1,1}
 	local _highlightColor = {1,1,0,1}
 	local _label = ""
