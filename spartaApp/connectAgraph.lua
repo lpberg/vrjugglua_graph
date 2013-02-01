401634d9a7e36ca5a7fc0a9ad97ba8efb1efcdd0
Leif Berg <lpberg@iastate.edu>
2013-02-01 15:20:06 -0600

    recommented lines in for full functionality

--- spartaApp/connectAgraph.lua
+++ spartaApp/connectAgraph.lua
@@ -5,18 +5,22 @@ vrjLua.appendToModelSearchPath(getScriptFilename())
 dofile(vrjLua.findInModelSearchPath([[..\graph\loadGraphFiles.lua]]))
 dofile(vrjLua.findInModelSearchPath([[MySounds/PlaySound.lua]]))
 dofile(vrjLua.findInModelSearchPath([[..\graph\simpleLightsGraph.lua]]))
+
 local wavPath = vrjLua.findInModelSearchPath("tiny.wav")
 local myWavSound = SoundWav(wavPath)
 local wavPath2 = vrjLua.findInModelSearchPath("tiny1.wav")
 local myWavSound2 = SoundWav(wavPath2)
 local wavPath3 = vrjLua.findInModelSearchPath("new.wav")
 local myWavSound3 = SoundWav(wavPath3)
+
+my_radius = .13/2
+
 g = Graph(
 	{
-		["one"] = GraphNode{position = {0,0,0},radius = .13};
-		["two"] = GraphNode{position = {0,1,0},radius = .13};
-		["three"] = GraphNode{position = {1,1,0},radius = .13};
-		["four"] = GraphNode{position = {1,0,0},radius = .13};
+		["one"] = GraphNode{position = {0,0,0},radius = my_radius};
+		["two"] = GraphNode{position = {0,1,0},radius = my_radius};
+		["three"] = GraphNode{position = {1,1,0},radius = my_radius};
+		["four"] = GraphNode{position = {1,0,0},radius = my_radius};
 
 	},
 	{
@@ -25,17 +29,35 @@ g = Graph(
 	}
 )
 RelativeTo.World:addChild(g.osg.root)
+local device = gadget.PositionInterface("VJWand")
 
+
+xform = osg.MatrixTransform()
+xform:addChild(Sphere{radius = .05})
+RelativeTo.World:addChild(xform)
+Actions.addFrameAction(
+	function()
+		while true do
+			mat = xform:getMatrix()
+			mat:setTrans(RelativeTo.World:getInverseMatrix():preMult(device.position))
+			xform:setMatrix(mat)
+			Actions.waitForRedraw()
+		end
+
+	end
+)
+
+--HOVER ACTION HIGHLIGHT
 local hoveredNode = nil
 Actions.addFrameAction(
 	function()
-		local device = gadget.PositionInterface("VJWand")
 		while true do
-			local device_pos = device.position - osgnav.position
+			local device_pos = RelativeTo.World:getInverseMatrix():preMult(device.position)
 			local changed_this_time = false
 			for _,node in ipairs(g.nodes) do
 				local distance = (osg.Vec3d(unpack(node.position))-device_pos):length()
-				if math.abs(distance) < (node.radius*2) then
+				-- print(distance)
+				if math.abs(distance) < (my_radius*3) then
 					if not node.isHighlighted then
 						node:highlight(true)
 						myWavSound2:trigger(1)
@@ -56,27 +78,26 @@ Actions.addFrameAction(
 		end
 	end
 )
-
+--ADD NODE TO SCENE
 Actions.addFrameAction(function()
 	local drawBtn = gadget.DigitalInterface("WMButtonPlus")
-	local device = gadget.PositionInterface("VJWand")
 	while true do
 		repeat
 			Actions.waitForRedraw()
 		until drawBtn.justPressed
-		local newPos = device.position - osgnav.position
+		local newPos = RelativeTo.World:getInverseMatrix():preMult(device.position)
 		local newName = tostring(newPos:x()+math.random(1,100))
 		g:addNodes{
-			[newName] = GraphNode{position = {newPos:x(),newPos:y(),newPos:z()+.1},radius = .13};
+			[newName] = GraphNode{position = {newPos:x(),newPos:y(),newPos:z()+.1},radius = my_radius};
 		}
 		myWavSound3:trigger(1)
 	end
 end)	
 
+--ADD NODE TO SCENE
 Actions.addFrameAction(function()
 	local tempXFORM = Transform{}
 	local drawBtn = gadget.DigitalInterface("VJButton1")
-	local device = gadget.PositionInterface("VJWand")
 	while true do
 		local pointOne = nil
 		local pointTwo = nil
@@ -86,7 +107,7 @@ Actions.addFrameAction(function()
 		pointOne = hoveredNode
 		RelativeTo.World:addChild(tempXFORM)
 		while drawBtn.pressed and pointOne ~= nil do 
-			local device_pos = device.position - osgnav.position
+			local device_pos = RelativeTo.World:getInverseMatrix():preMult(device.position)
 			tempXFORM.Child[1] = CylinderFromHereToThere(osg.Vec3d(unpack(pointOne.position)), device_pos, pointOne.radius/3, {1,1,0,1}) 
 			Actions.waitForRedraw()
 		end
