3c54b2fee0d386b04499016381a96048a2dc66bc
Leif Berg <lpberg@iastate.edu>
2013-05-05 14:22:17 -0500

    added 'useful' geometry for a edge with a 'cone' arrow on the end.

--- graph/UsefulGeometry.lua
+++ graph/UsefulGeometry.lua
@@ -58,6 +58,57 @@ CylinderFromHereToThere = function(here, there, myradius, mycolor)
 	return xform
 end
 
+Cone = function(a)
+	local pos = osg.Vec3(0.0, 0.0, 0.0)
+	if a.position then
+		pos:set(unpack(a.position))
+	end
+	local drbl = osg.ShapeDrawable(osg.Cone(pos, a.radius or 1.0,a.height or 1.0))
+	local color = osg.Vec4(0,0,0,0)
+	if a.color then
+		color:set(unpack(a.color))
+	end
+	drbl:setColor(color)
+	local geode = osg.Geode()
+	geode:addDrawable(drbl)
+	return geode
+end
+
+CylinderFromHereToThereWithArrows = function(a) 
+	local midpoint = (a.here + a.there) * 0.5
+	local total_dist = (a.here-a.there):length()
+	if a.cone_color == nil then
+		a.cone_color = a.color
+	end
+	if a.cone_radius == nil then
+		a.cone_radius = a.radius*1.5
+	end
+	if a.cone_height == nil then
+		a.cone_height = total_dist*(1/15)
+	end
+	
+	local cylinder_lenth = total_dist - a.node_radius - a.cone_height 
+	local cone_z = (cylinder_lenth-(total_dist/2.0))+(a.cone_height/4)
+	local col_z = -((total_dist-cylinder_lenth)/2.0)
+	
+	local cone = Transform{
+		Cone{position = {0,0,cone_z},color=a.cone_color, radius = a.cone_radius, height=a.cone_height},
+	}
+	local col = Transform{
+		position = {0,0,col_z},
+		Cylinder{height = cylinder_lenth, color = a.color, radius = a.radius},
+	}
+	local xform = Transform{
+		position = {midpoint:x(),midpoint:y(),midpoint:z()},
+		col,
+		cone,
+	}
+	local newVec = a.there - a.here
+	local newQuat = osg.Quat()
+	newQuat:makeRotate(osg.Vec3(0,0,1),osg.Vec3(newVec:x(),newVec:y(),newVec:z()))
+	xform:setAttitude(newQuat)
+	return xform
+end
 
 RedIndicatorSphere = function(myRadius) 
 	local xform = Transform{
