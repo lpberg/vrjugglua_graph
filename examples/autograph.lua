071536cf4b099d5c4eab0dcc7038ae590aa2ed39
Leif Berg <lpberg@iastate.edu>
2013-05-09 15:56:26 -0500

    cleaned up autograph.lua

--- examples/autograph.lua
+++ examples/autograph.lua
@@ -9,50 +9,54 @@ runfile([[..\extras\FDG_Layout.lua]])
 
 
 -- A graph is G=(V, E)
-g = Graph({},{})
 
-local function randColor()
-	local r = osgLua.GLfloat(math.random(-100,100)/100)
-	local b = osgLua.GLfloat(math.random(-100,100)/100)
-	local g = osgLua.GLfloat(math.random(-100,100)/100)
-	local a = osgLua.GLfloat(math.random(-100,100)/100)
-	return {r,g,b,a}
-end
+local function randomGraph()
+	local g = Graph({},{})
 
-local randomNodeNum = 1
-local function addRandomNode(graph)
-	local nodename = ("random_%d"):format(randomNodeNum)
-	graph:addNodes{
-		[nodename] = GraphNode{position = {math.random(-2,2), math.random(-2,2), math.random(-2,2)}}
-		-- [nodename] = GraphNode{position = {math.random(-2,2), math.random(-2,2), math.random(-2,2)},color = randColor()}
-	}
-	randomNodeNum = randomNodeNum + 1
-end
-
-function addRandomEdge(graph)
-	local n = #(graph.nodes)
-	local fromNum = math.random(1, n)
-	local toNum = math.random(1, n)
-	if fromNum ~= toNum then
-		graph:addEdges{
-			DirectedEdge(graph.nodes[fromNum].name, graph.nodes[toNum].name,{radius=.1/3})
-		}
-	else
-		print "Whoops, rolled the same number twice. Not actually adding an edge."
+	local function randColor()
+		local r = osgLua.GLfloat(math.random(-100,100)/100)
+		local b = osgLua.GLfloat(math.random(-100,100)/100)
+		local g = osgLua.GLfloat(math.random(-100,100)/100)
+		local a = 1
+		return {r,g,b,a}
 	end
+
+	local randomNodeNum = 1
+	local function addRandomNode(graph)
+		local nodename = ("random_%d"):format(randomNodeNum)
+		graph:addNodes{
+			[nodename] = GraphNode{position = {math.random(-2,2), math.random(-2,2), math.random(-2,2)},color=randColor()}
+		}
+		randomNodeNum = randomNodeNum + 1
+	end
+
+	function addRandomEdge(graph)
+		local n = #(graph.nodes)
+		local fromNum = math.random(1, n)
+		local toNum = math.random(1, n)
+		if fromNum ~= toNum then
+			graph:addEdges{
+				DirectedEdge(graph.nodes[fromNum].name, graph.nodes[toNum].name,{radius=.1/3})
+			}
+		else
+			print "Whoops, rolled the same number twice. Not actually adding an edge."
+		end
+	end
+
+	-- Add some random nodes
+	for i=1,10 do
+		addRandomNode(g)
+	end
+
+	-- Add some random edges
+	for i=1,10 do
+		addRandomEdge(g)
+	end
+	return g
 end
 
--- Add some random nodes
-for i=1,10 do
-	addRandomNode(g)
-end
-
--- Add some random edges
-for i=1,10 do
-	addRandomEdge(g)
-end
-
-RelativeTo.World:addChild(g.osg.root)
+graph = randomGraph()
+RelativeTo.World:addChild(graph.osg.root)
 
 local args = {}
 -- args.c_mult = 19
